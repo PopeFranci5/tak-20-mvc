f616c912c75008d7dacd78e4c7c505633f4986a8
Nauka <andrus.naulainen@gmail.com>
2022-03-28 11:47:45 +0300

    Get and post routes

--- routes.php
+++ routes.php
@@ -4,5 +4,12 @@ $router->define([
     '' => 'controllers/index.php',
     'about' => 'controllers/about.php',
     'about/culture' => 'controllers/about-culture.php',
-    'contact' => 'controllers/contact.php'
+    'contact' => 'controllers/contact.php',
+    'location' => 'controllers/add-location.php'
 ]);
+
+$router->get('', 'controllers/index.php');
+$router->get('about', 'controllers/about.php');
+$router->get('about/culture', 'controllers/about-culture.php');
+$router->get('contact', 'controllers/contact.php');
+$router->post('location', 'controllers/add-location.php');
--- views/index.view.php
+++ views/index.view.php
@@ -1,5 +1,14 @@
 <?php require_once('partials/head.php'); ?>
 
+<h1>Sisesta asukoht</h1>
+
+<form action="/location" method="POST">
+    <input type="text" name="location">
+    <button type="submit">Sisesta</button>
+</form>
+
+
+
 <ul>
     <?php foreach ( $tasks as $task ) : ?>
         
